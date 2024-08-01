573c6bf8349cd98879cb3af272fa86abd8dd2c24
João <Joaopauloresendecosta55@gmail.com>
2024-08-01 19:23:16 -0300

    Atualização do projeto

--- SpringProject-main/ScreenMatch (1)/ScreenMatch/src/main/java/br/com/JavaProject/ScreenMatch/ScreenMatchApplication.java
+++ SpringProject-main/ScreenMatch (1)/ScreenMatch/src/main/java/br/com/JavaProject/ScreenMatch/ScreenMatchApplication.java
@@ -1,12 +1,16 @@
 package br.com.JavaProject.ScreenMatch;
 
 import br.com.JavaProject.ScreenMatch.model.DataEpisodes;
+import br.com.JavaProject.ScreenMatch.model.DataSeason;
 import br.com.JavaProject.ScreenMatch.model.DataSeries;
 import br.com.JavaProject.ScreenMatch.server.DataConvert;
 import br.com.JavaProject.ScreenMatch.server.server;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenMatchApplication implements CommandLineRunner {
@@ -29,6 +33,15 @@ public class ScreenMatchApplication implements CommandLineRunner {
 		DataEpisodes dadosEpisodio = conversor.obterDados(json, DataEpisodes.class);
 		System.out.println(dadosEpisodio);
 
+		List<DataSeason> temporadas = new ArrayList<>();
+
+		for (int i = 1; i<=dados.totalTemporadas(); i++) {
+			json = Server.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=5dd27116");
+			DataSeason dadosTemporada = conversor.obterDados(json, DataSeason.class);
+			temporadas.add(dadosTemporada);
+
+		}
+		temporadas.forEach(System.out::println);
 
 
 
